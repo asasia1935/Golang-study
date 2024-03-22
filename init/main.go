22b7b4276d834ae39ed96c31f4bba690fe0e3db1
asasia1935 <sungmin1935@naver.com>
2024-03-22 16:20:37 +0900

    내장 패키지를 통한 api handler

--- init/main.go
+++ init/main.go
@@ -1,7 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 // 시작점
 
 func main() {
+	http.HandleFunc("/", helloWorld) // 내장 패키지 활용
 
+	// 서버가 죽지 않고 켜두는 용도
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("에러가 떴습니다.")
+		panic(err)
+	}
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("hello world")
 }
